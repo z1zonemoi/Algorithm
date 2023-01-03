ea97383b1acca50eb5862b3040a19a1297968982
Yujung Choi <107454269+z1zonemoi@users.noreply.github.com>
2023-01-03 09:45:28 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
