1cef9b53cb8b624230c11c92119f292a2d7ba95a
Yujung Choi <107454269+z1zonemoi@users.noreply.github.com>
2023-01-03 15:05:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.14 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,22 @@
+function solution(participant, completion) {
+    var answer = '';
+    let result = {};
+    let totalPeople = participant.map(name=>{
+        if(result[name]){
+            return result[name] = result[name] + 1
+        }
+        return result[name] = 1
+    });
+    completion.map(name => {
+        return result[name] = result[name] - 1;
+    });
+    for(const key in result){
+        if(result[key] === 1) {
+            return answer = key;
+            break;
+        }
+    }
+    
+    
+    return answer;
+}
