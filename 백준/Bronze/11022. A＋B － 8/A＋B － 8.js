2a94d5a878946bb51a40ff815a493ddbc8e75568
Yujung Choi <107454269+z1zonemoi@users.noreply.github.com>
2023-01-11 17:01:09 +0900

    [Bronze V] Title: A+B - 8, Time: 140 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+let T = +input[0];
+
+let result = [];
+
+for (let i = 0; i < T; i++) {
+  let [a, b] = input[i + 1].split(" ").map(Number);
+  result.push(`Case #${i + 1}: ${a} + ${b} = ${a + b}`);
+}
+
+console.log(result.join("\n"));
