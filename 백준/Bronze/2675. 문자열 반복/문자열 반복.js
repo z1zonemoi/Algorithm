11bb23c450b70db009d9b5ffd5886e7d9399c230
yujung Choi <107454269+z1zonemoi@users.noreply.github.com>
2023-01-16 23:02:12 +0900

    [Bronze II] Title: 문자열 반복, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,16 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let result = input.slice(1).map((value) => {
+  let [number, string] = value.split(" ");
+  let repeat = string
+    .split("")
+    .map((value) => value.repeat(number))
+    .join("");
+  return repeat;
+});
+
+console.log(result.join("\n"));
