4090348cedc33f295325ddd2c943c4bfe955ccae
yujung Choi <107454269+z1zonemoi@users.noreply.github.com>
2023-01-19 13:41:45 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1496 ms, Memory: 215512 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,11 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number)
+  .slice(1);
+
+input.sort((a, b) => a - b);
+
+console.log(input.join("\n"));
