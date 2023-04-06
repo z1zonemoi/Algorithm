396991f91fb056bbfd890511cb7aa2e3305f9759
Yujung Choi <107454269+z1zonemoi@users.noreply.github.com>
2023-04-07 00:54:20 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,22 @@
+function solution(a, b) {
+    const week = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+    const day30 = [4,6,9,11];
+    const day31 = [1,3,5,7,8,10,12];
+    
+    let days = 0;
+    let count = 1;
+    
+    while(count < a){
+        if(count === 2)  days += 29;
+        if(day31.includes(count))  days += 31;
+        if(day30.includes(count))  days += 30;
+        count ++
+    }
+    
+    days += b;
+    
+    const rest = days % 7;
+    
+    
+    return rest ? week[rest-1] : "THU"
+}
